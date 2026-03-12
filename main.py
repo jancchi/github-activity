da7e5e194fd009cb261509e13e4657ab8f8db70c
jancchi <jancchi.the.rock@gmail.com>
2026-03-12 07:07:35 +0100

    Remove redundant code, add plotly visualization

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
 import requests
+import plotly.graph_objects as go
+from collections import Counter
+from datetime import datetime
 
+
+def visualize_commits(commits):
+    dates = [x["commit"]["author"]["date"][:10] for x in commits]
+    counts = Counter(dates)
+
+    sorted_dates = sorted(counts.keys())
+    sorted_counts = [counts[d] for d in sorted_dates]
+
+    fig = go.Figure(go.Bar(x=sorted_dates, y=sorted_counts))
+    fig.update_layout(
+        title="Commit activity",
+        xaxis_title="Date",
+        yaxis_title="Commits",
+        xaxis_tickangle=-45
+    )
+    fig.show()
+
+
+def visualize_contributors(contributors):
+    logins = [x["login"] for x in contributors[:10]]
+    contribs = [x["contributions"] for x in contributors[:10]]
+
+    fig = go.Figure(go.Bar(
+        x=contribs,
+        y=logins,
+        orientation="h"
+    ))
+    fig.update_layout(
+        title="Top contributors",
+        xaxis_title="Commits",
+        yaxis=dict(autorange="reversed")
+    )
+    fig.show()
 
 def get_owner():
     while True:
@@ -71,41 +107,16 @@ def main():
                     break
                 elif choice == "1":
                     commits = requests.get(url+ "/commits").json()
-                    print(commits[0]["commit"]["author"].keys())
-                    print("\n".join([x["commit"]["author"]["date"] + " " + x["commit"]["author"]["name"] + " " + x["commit"]["message"].split('\n')[0] for x in commits[:5]]))
+                    visualize_commits(commits)
+                    #print(commits[0]["commit"]["author"].keys())
+                    #print("\n".join([x["commit"]["author"]["date"] + " " + x["commit"]["author"]["name"] + " " + x["commit"]["message"].split('\n')[0] for x in commits[:5]]))
                 elif choice == "2":
                     contributors = requests.get(url+ "/contributors").json()
-                    print(contributors[0].keys())
-                    print("\n".join([x["login"] for x in contributors[:5]]))
+                    visualize_contributors(contributors)
+                    #print(contributors[0].keys())
+                    #print("\n".join([x["login"] for x in contributors[:5]]))
         else:
             print("Wrong choice")
-
-
-    repo = input("Enter a GitHub repository: ")
-
-    url = f"https://api.github.com/repos/{owner}/{repo}"
-
-    print(requests.get(url).json())
-
-    # 1. Get List of Contributors (Authors)
-    contributors_url = f"{url}/contributors"
-    authors_res = requests.get(contributors_url)
-
-    if authors_res.status_code == 200:
-        authors = authors_res.json()
-        print(f"Top Author: {authors[0]['login']} with {authors[0]['contributions']} commits")
-
-    # 2. Get Commit History
-    commits_url = f"{url}/commits"
-    commits_res = requests.get(commits_url)
-
-    if commits_res.status_code == 200:
-        latest_commits = commits_res.json()
-        for commit in latest_commits[:5]:  # Show first 5
-            message = commit['commit']['message'].split('\n')[0]
-            date = commit['commit']['author']['date']
-            print(f"[{date}] {message}")
-
 
 if __name__ == "__main__":
     main()
